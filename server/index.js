cddd3b82d07abacd365e4d45cbe4885f38670a9a
sharanyanjs <sharanyanjs@gmail.com>
2025-04-06 14:58:50 +0530

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is up and running!");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
